62629770c91c019a3bf677429ba12259111bd47e
Simone <simone.brunelli3@studio.unibo.it>
2025-01-04 01:06:05 +0100

    1LineSol

--- Reverse/inverti.sh
+++ Reverse/inverti.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-mapfile -t righe < "inverti.txt"  # Legge tutte le righe in un array
-for ((indice=${#righe[@]}-1; indice>=0; indice--)); do
-    echo "${righe[indice]}"
-done
+tac "inverti.txt"
