b67ad7995d36873318db4d4362ff4f30d30f4939
Simone <simone.brunelli3@studio.unibo.it>
2025-01-12 16:39:18 +0100

    fix

--- Stringhe/stringhe_confinate.sh
+++ Stringhe/stringhe_confinate.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-tempfile=$(mktemp)
-
 while read line ; do
-	last=${line#\"*\"*\"*\"}	#Cancella dall'inzio
-	res=${last%*\"*\"*}			#Cancella dalla fine
+	last=${line#\"*\"*\"*\"*\"}	#Cancella dall'inzio
+	res=${last%\"*\"*\"*}			#Cancella dalla fine
 	num=`grep "$res" stringhe.txt | wc -l`
-	echo $res $num >> $tempfile
-done < stringhe.txt
+	echo $res $num
+done < stringhe.txt | sort | uniq 
 
-
-sort -k1 -n -r $tempfile | uniq
 
